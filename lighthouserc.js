dd62275021f4f74e41b4279dc04e49850fbe3d04
Troy Dieter <troydieter@gmail.com>
2022-05-12 22:35:51 -0400

    added lighthouse file for an example

---
+++ lighthouserc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    ci: {
+    collect: {
+        url: "https://www.example.com",
+        maxAutodiscoverUrls: 3,
+        numberOfRuns: 2,
+        settings: {
+            chromeFlags: "--no-sandbox",
+            onlyCategories: ["performance", "best-practices", "accessibility", "seo"],
+            skipAudits: ['uses-http2', 'uses-long-cache-ttl', 'link-text']
+        }
+    },
+    upload: {
+        target: 'lhci',
+        serverBaseUrl: 'https://lhci.example.com',
+        token: 'example-000-example',
+        ignoreDuplicateBuildFailure: true,
+        allowOverwriteOfLatestBranchBuild: true
+    },
+    },
+};
