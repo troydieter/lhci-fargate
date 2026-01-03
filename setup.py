e33f5223dd8084422efe63e7948be4191bbe562c
Troy Dieter <troydieter@gmail.com>
2026-01-03 04:28:04 +0000

    Step 13: Update project documentation and create Python setup files Build status: Failed (expected - dependencies not installed)

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lhci-fargate",
+    version="1.70",
+    description="Deployment of Lighthouse CI through AWS-CDK onto AWS Fargate",
+    packages=find_packages(),
+    install_requires=[
+        "aws-cdk-lib==2.233.0",
+        "constructs==10.4.4",
+        "cdk-watchful==3.6.0"
+    ],
+    python_requires=">=3.8",
+)
