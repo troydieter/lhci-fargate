7d453176f8247ede0ee3b91131117c6a21593b4f
ATX Executor <atx-executor@aws.amazon.com>
2026-01-04 18:02:16 -0500

    added description

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import aws_cdk as cdk
 from lhci_stack import LHCIStack
 
 app = cdk.App()
-LHCIStack(app, "LHCIStack", 
+LHCIStack(app, "LHCIStack", description="Lighthouse CI",
     env=cdk.Environment(
         account=os.environ.get("CDK_DEFAULT_ACCOUNT"),
         region=os.environ.get("CDK_DEFAULT_REGION")
